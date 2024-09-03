14ae9d4ae410e82643dc3a1f2209266756b38dde
meetpatel347 <48251716+meetpatel347@users.noreply.github.com>
2024-09-03 22:49:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,11 @@ st.write( 'The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session ()
-my_dataframe = session. table("smoothies.public.fruit_options"). select (col('FRUIT_NAME'))
+my_dataframe = session. table("smoothies.public.fruit_options"). select (col('FRUIT_NAME'),col('Search_on'))
 #st.dataframe (data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:'
@@ -42,7 +45,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
         st.subheader(fruit_chosen + ' Nutrition Information' )
+        search_on=pd_df. loc[pd_df [' FRUIT_NAME' ] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        st.write ('Your favorite fruit is:', option)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
